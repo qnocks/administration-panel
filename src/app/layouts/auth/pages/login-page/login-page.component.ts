fbf50d55001871e177127fc331d1ca2f3d404e99
qnocks <roma.ost2012@mail.ru>
2022-03-04 22:32:57 +0300

    PSAP-01-implementation-of-login-layout

---
+++ src/app/layouts/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,34 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'psap-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const request = this.loginForm.value;
+      console.log(request)
+      // TODO: call service to login with provided request
+    }
+  }
+
+  private buildForm() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+}
---
+++ src/app/layouts/auth/pages/login-page/login-page.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from "@angular/core";
+import {LoginPageComponent} from "./login-page.component";
+import {CommonModule} from "@angular/common";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [
+    LoginPageComponent
+  ],
+  imports: [
+    RouterModule.forChild([{path: '', component: LoginPageComponent}]),
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    LoginPageComponent
+  ]
+})
+export class LoginPageModule { }
